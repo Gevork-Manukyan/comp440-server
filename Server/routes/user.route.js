2952792c9aec15c0e6377c6cd686fb424536d695
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-11 15:09:12 -0800

    adds user.route.js and userController.js files

---
+++ Server/routes/user.route.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const router = express.Router()
+const userController = require("../controllers/userController")
+
+
+// Login Endpoint
+router.post("/login", async (req, res) => {
+    const response = await userController.login(req, res)
+
+    if (response === false) {
+        res.status(401).send({ error: "Email or password is invalid." })
+    } else {
+        res.status(200).send(response)
+    }
+})
+
+// Register Endpoint 
+router.post("/register", async (req, res) => {
+    const response = await userController.register(req, res)
+    res.status(200).send(response)
+})
+
+module.exports = router

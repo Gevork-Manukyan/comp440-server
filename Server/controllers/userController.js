fe55b670993bba717b3ebc307429cc380700c6e5
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-11 16:40:03 -0800

    connects to mysql db

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model")
+const db = require("../../db")
 
 const login = async (req, res) => {
     // const { email, password } = req.body
---
+++ db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2')
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Manukyan11@',
+  database: 'test'
+})
+
+db.connect((err) => {
+  if (err) throw err
+  console.log("Connected to Database!")
+})
+
+db.query('SELECT * FROM course', (err, result, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', result)
+})
+
+// connection.end()
+
+module.exports = db

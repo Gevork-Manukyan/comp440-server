024b02416b70b041e3f99600dfe90f03d0f81106
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-04-23 22:06:59 -0700

    adds items route routes

--- Server/routes/items.route.js
+++ Server/routes/items.route.js
@@ -2,7 +2,38 @@ const express = require("express")
 const router = express.Router()
 const itemsController = require("../controllers/itemsController")
 
-
+router.get('/', async (req, res) => {
+    try {
+      const items = await Item.findAll();
+      res.send(items);
+    } catch (err) {
+      console.error('Error fetching items:', err);
+      res.status(500).send({ error: { message: 'An error occurred while fetching items', details: err } });
+    }
+  });
+  
+router.get('/search', async (req, res) => {
+    const { category_startswith } = req.query;
+    try {
+        let items;
+        if (!category_startswith) {
+            items = await Item.findAll();
+        } 
+        else {
+            items = await Item.findAll({
+                where: {
+                category: {
+                    [sequalize.like]: `${category_startswith}%`
+                }
+                }
+            });
+        }
+        res.send(items);
+    } catch (err) {
+        console.error('Error fetching items with specified category:', err);
+        res.status(500).send({ error: { message: 'An error occurred while fetching items with the specified category' } });
+    }
+});
 
 
 module.exports = router

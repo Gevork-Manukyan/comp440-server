060ed95ee2cdae0ae002031fdd66d3743d346e52
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-04-24 15:13:03 -0700

    fixes droping

--- Server/controllers/dbController.js
+++ Server/controllers/dbController.js
@@ -2,11 +2,29 @@ const sequelize = require("../db");
 const Item = require("../models/item.model");
 const User = require("../models/user.model");
 const Review = require("../models/review.model")
+const Category = require("../models/category.model")
 
 const initDB = () => {
-    sequelize.sync({ force: true })
+    sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
     .then(() => {
-      console.log('Users table created');
+        return sequelize.query('DROP TABLE IF EXISTS reviews');
+    })
+    .then(() => {
+        return sequelize.query('DROP TABLE IF EXISTS item_category');
+    })
+    .then(() => {
+        return sequelize.query('DROP TABLE IF EXISTS items');
+    })
+    .then(() => {
+        return sequelize.query('DROP TABLE IF EXISTS categories');
+    })
+    .then(() => {
+      return sequelize.query('DROP TABLE IF EXISTS users');
+    })
+    .then(() => {
+      return sequelize.sync({ force: true })
+    })
+    .then(() => {
 
         User.create({ username: "JohnDoe123", email: "JohnDoe@gmail.com", password: "password123", firstName: "John", lastName: "Doe" });
         User.create({ username: "BillyDoe123", email: "BillyDoe@gmail.com", password: "password123", firstName: "Billy", lastName: "Doe" });
@@ -15,24 +33,31 @@ const initDB = () => {
         User.create({ username: "BrianDoe123", email: "BrianDoe@gmail.com", password: "password123", firstName: "Brian", lastName: "Doe" });
 
 
-        Item.create({
-          title: 'Example Item',
-          description: 'This is an example item.',
-          price: 10.99
-        }).then(item => {
-          console.log('Item created:');
-          Review.create({
-            rating: 'Excellent',
-            reviewDescription: 'This is a great product',
-            itemId: item.id
-          }).then(review => {
-            console.log('Review created:');
-          }).catch(error => {
-            console.error('Error creating review:', error);
-          })
-        }).catch(error => {
-          console.error('Error creating item:', error);
-        })
+        try {
+          const item = Item.create({
+            title: "Example Item",
+            description: "This is an example item.",
+            price: 10.99,
+          });
+          console.log("Item created:");
+        
+          const review = Review.create({
+            rating: "Excellent",
+            reviewDescription: "This is a great product",
+            itemId: item.id,
+          });
+          console.log("Review created:");
+
+          const newCategory = Category.create({
+            name: 'New Category'
+          });
+
+
+          
+        } catch (error) {
+          console.error("Error creating item and/or review:", error);
+        }
+        
 
     })
     .catch((error) => {

c9a45bd15bb24e86cf5e8dbbeeaee989c5da7209
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-04-24 16:08:11 -0700

    updates getallitems to use ORM functions

--- Server/controllers/itemsController.js
+++ Server/controllers/itemsController.js
@@ -4,15 +4,28 @@ const Category = require("../models/category.model");
 const sequelize = require("../db");
 
 async function getAllItems() {
-  const result = await sequelize.query(`
-  SELECT title, description, datePosted, price, userUsername, category FROM items
-  JOIN itemCategories ON items.id = itemCategories.itemId
-  JOIN categories ON categories.id = itemCategories.categoryId
-  `)
+  const items = await Item.findAll({
+    attributes: ['id', 'title', 'description', 'datePosted', 'price'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category'],
+        through: {
+          attributes: []
+        }
+      }
+    ]
+  });
 
-  console.log(result)
-  return result
+  const result = items.map(item => {
+    const itemData = item.toJSON();
+    itemData.categories = itemData.categories.map(category => category.category);
+    return itemData;
+  });
+
+  return result;
 }
+
 
 async function getAllWhere(categoryName) {
   return await Item.findAll({
